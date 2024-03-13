88e91225b890bb262fdff350d690a74da7a357a4
hallow0312 <leejaehyun0312@naver.com>
2024-03-13 21:46:44 +0900

    2024.03.13.단방향연결리스트

---
+++ Class1th(TimeComplexity)/Source1.cpp
@@ -0,0 +1,159 @@
+﻿//#include<iostream>
+//
+//using namespace std;
+//
+//typedef struct Node
+//{
+//	int data;
+//	Node* next;
+//	~Node()
+//	{
+//		cout << "Release Node" << endl;
+//	}
+//
+//}Node;
+//
+//void PushFront(Node* target, int data)
+//{
+//	Node* newNode = new Node;
+//	newNode->data = data;
+//	newNode->next = target->next;
+//	target->next = newNode;
+//
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//int main()
+//{
+//#pragma region 시간 복잡도
+//	// 컴퓨터 프로그램의 입력값과 연산 수행 시간의 
+//	// 상관 관계를 나타내는 척도.
+//
+//	// Big-O 표기법
+//	// 인수가 특정 값이나 무한대로 향하는 경향이 
+//	// 있을 때 함수의 제한 동작을 설명하는 수학적 표기법.
+//
+//	// O(1) : constant Complexity
+//	// 입력 값이 증가하더라도 시간이 늘어나지 않는 시간 복잡도 .
+//
+//	// ex) 배열의 원소 접근 , vector 의 push_back, pop_back
+//
+//	// O(n) : linear complexity 
+//	// 입력 값이 증가함에 따라 시간 또한 같은 비율로 
+//	// 증가하는 시간 복잡도 .
+//
+//	//ex) for문 반복문
+//
+//	// O(log n) : logarithmic time
+//	// 입력값이 커질 때 연산 횟수가 log n 에 비례해서 증가하는
+//	// 시간복잡도 .
+//
+//	// ex) 이진 검색 트리 
+//
+//	// O(n²) : Quadratic Complexity
+//	// 입력 값이 증가함에 따라 시간이 n의 제곱수의 
+//	// 비율로 증가하는 시간 복잡도 .
+//
+//	// ex) 2중 for 문 
+//
+//
+//
+//
+//
+//
+//#pragma endregion
+//
+//#pragma region 연결 리스트
+//#pragma region 연결리스트 구조체로만
+//
+//
+//
+//	//Node* head = new Node;  
+//	//head->data = 0;
+//	//head->next = nullptr;
+//
+//	//Node* node1 = new Node;
+//
+//	//node1->data = 10;
+//	//Node* node2 = new Node;
+//	//node2->data = 20;
+//
+//
+//	//head->next = node1;
+//	//node1->next = node2;
+//
+//	//node2->next = nullptr;
+//
+//	//cout <<"head->next : " << head->next << endl;
+//	//cout << "node1 : " << node1 << endl << endl;
+//	//cout << "node1->next :" << node1->next << endl;
+//	//cout <<"node2 :" << node2 << endl << endl;
+//	//
+//	//cout << head->next->data<<endl;
+//	//cout << node1->next->data << endl<<endl;
+//	//
+//	//cout << "node2->next : " << node2->next << endl;
+//
+//	//delete(head);
+//	//delete(node1);
+//	//delete(node2);
+//
+//#pragma endregion	
+//
+//#pragma region 연결리스트 함수사용
+//	Node* head = new Node;
+//	head->next = nullptr;
+//
+//	//1.currentNode 가 Null 일 때까지 반복
+//
+//	PushFront(head, 100);
+//	PushFront(head, 99);
+//	PushFront(head, 98);
+//	Node* currentNode = head->next;
+//
+//
+//	while (currentNode != NULL)
+//	{
+//
+//		cout << currentNode->data << endl;
+//
+//		currentNode = currentNode->next;
+//
+//
+//	}
+//
+//
+//	currentNode = head;
+//	while (head != NULL)
+//	{
+//
+//		head = currentNode->next;
+//
+//		delete currentNode;
+//		currentNode = head;
+//	}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//#pragma endregion
+//
+//#pragma endregion
+//
+//
+//
+//	return 0;
+//}
---
+++ Class2th(SingleLinkedList)/Source2.cpp
@@ -0,0 +1,146 @@
+﻿//#include<iostream>
+//
+//using namespace std;
+//
+//template <typename T>
+//
+//class SingleLinkedList
+//{
+//private:
+//	struct Node
+//	{
+//		T data;
+//		Node* next;
+//	};
+//	Node* head;
+//	Node* tail;
+//	int size;
+//public:
+//	SingleLinkedList()
+//	{
+//		head = nullptr;
+//		tail = nullptr;
+//		size = 0;
+//	}
+//
+//	~SingleLinkedList()
+//	{
+//		while (size == 0)
+//		{
+//			PopFront();
+//
+//		}
+//
+//		cout << "Release List" << endl;
+//	}
+//
+//	void PushFront(T data)
+//	{
+//		Node* newNode = new Node;
+//		newNode->data = data;
+//		newNode->next = nullptr;
+//		if (head == nullptr && tail == nullptr)
+//		{
+//			head = newNode;
+//			tail = newNode;
+//		}
+//		else
+//		{
+//			newNode->next = head;
+//			head = newNode;
+//		}
+//		size++;
+//	}
+//
+//	void PushBack(T data)
+//	{
+//		Node* newNode = new Node;
+//		newNode->data = data;
+//		newNode->next = nullptr;
+//		if (head == nullptr && tail == nullptr)
+//		{
+//			head = newNode;
+//			tail = newNode;
+//		}
+//		else
+//		{
+//
+//			tail->next = newNode;
+//			tail = newNode;
+//		}
+//		size++;
+//	}
+//	void PopFront()
+//	{
+//		cout << "Remaining Node : " << size << endl;
+//		if (head == nullptr && tail == nullptr)
+//		{
+//			cout << "List is empty";
+//			return;
+//		}
+//
+//		Node* deleteNode = nullptr;
+//
+//
+//		if (size == 0)
+//		{
+//
+//			deleteNode = head;
+//			head = nullptr;
+//			tail = nullptr;
+//			delete deleteNode;
+//
+//		}
+//
+//		else
+//		{
+//			deleteNode = head;
+//			head = deleteNode->next;
+//			delete deleteNode;
+//
+//		}
+//		size--;
+//	}
+//
+//	void Show()
+//	{
+//		Node* currentNode = nullptr;
+//		currentNode = head;
+//		while (currentNode != NULL)
+//		{
+//			cout << currentNode->data << endl;
+//			currentNode = currentNode->next;
+//		}
+//	}
+//	int Size()
+//	{
+//		return size;
+//	}
+//};
+//
+//
+//int main()
+//{
+//	SingleLinkedList<int> SingleLinkedList;
+//
+//	SingleLinkedList.PushFront(100);
+//	SingleLinkedList.PushFront(99);
+//	SingleLinkedList.PushBack(98);
+//
+//	cout << "Single Linked List의 크기 : " << SingleLinkedList.Size() << endl;
+//
+//	SingleLinkedList.Show();
+//
+//
+//
+//
+//	return 0;
+//}
+//
+//
+//
+//
+//
+//
+//
+//
