2bbf49c62f52535064d44c16b961e45311a44418
hallow0312 <leejaehyun0312@naver.com>
2024-03-19 21:39:56 +0900

    2024.03.19.선형큐

--- ProgramLEE/ProgramLEE.cpp
+++ ProgramLEE/ProgramLEE.cpp
@@ -1,171 +1,16 @@
 ﻿#include<iostream>
 
 using namespace std;
-template <typename T>
-class CircleLinkedList
-{
-private:
-	struct Node
-	{
-		T data;
-		Node* next;
-	};
-	int size;
-	Node* head;
-public:
-	CircleLinkedList()
-	{
-		size = 0; 
-		head = nullptr;
-	} 
-	~CircleLinkedList()
-	{
-		while (size != 0)
-		{
-			PopFront();
-			
-		}
-		cout << "Release Node" << endl;
-		
-	}
-	void PushFront(T data)
-	{
-		Node* newNode = new Node;
-		newNode->data = data;
-		newNode->next = nullptr;
-		if (head == nullptr)
-		{
-			head = newNode;
-			newNode->next = head;
-			head->next = newNode;
-		}
-		else
-		{
-			newNode->next = head->next; 
-			head->next = newNode;
-			 
-		}
-		size++;
-
-	}
-	void PushBack(T data)
-	{
-		Node* newNode = new Node;
-		newNode->data = data;
-		newNode->next = nullptr;
-		if (head == nullptr)
-		{
-			head = newNode;
-			newNode->next = head;
-			head->next = newNode;
-		}
-		else
-		{
-			newNode->next = head->next;
-			head->next = newNode;
-			head = newNode; 
-		}
-		size++;
-	}
-	void PopFront()
-	{
-		cout << "Remaining Node : " << size << endl;
-		Node* deleteNode;
-		if (size == 0)
-		{	
-			
-			cout << "List is empty" << endl;
-			
-		}
-		else if (size == 1)
-		{
-			deleteNode = head;
-			delete deleteNode; 
-			head = nullptr;
-		}
-		else
-		{
-			deleteNode = head->next;
-			head->next=deleteNode->next;
-			delete deleteNode;
-		}
-		size--;
-
-	}
-	void PopBack()
-	{
-		cout << "Remaining Node : " << size << endl;
-		Node* deleteNode;
-		Node* currentNode; 
-		if (size == 0)
-		{
-			cout << "List is empty" << endl;
-		}
-		else if (size == 1)
-		{
-
-			deleteNode = head;
-			delete deleteNode;
-			head = nullptr;
-		}
-		else
-		{	
-			currentNode = head, deleteNode = head;
-			while (size - 1)
-			{
-				currentNode = currentNode->next;
-				
-			}
-			currentNode->next = head->next;
-			head = currentNode;
-			delete deleteNode; 
-				
-		}
-				
-		size--;
 
 
-	}
-		
-	void ShowNode()
-	{
-		Node* currentNode; 
-		currentNode = head;
-		int num = size+5;
-		cout <<endl<< "List : ";
-		while(num--)
-		{	
-			if (num > 1)
-			{
-				cout << currentNode->data << "-";
-				currentNode = currentNode->next;
-			}
-			if (num == 1)
-			{
-				cout << currentNode->data;
-				currentNode = currentNode->next;
-			}
-		}
-		cout << endl<<endl;
-		
-	}
 
-};
-		
 		
 int main()
 {
-	CircleLinkedList<int>circleLinkedList;
-	circleLinkedList.PushFront(10);
-	circleLinkedList.PushFront(20);
-	circleLinkedList.PushFront(30);
-	circleLinkedList.PushBack(40);
-	circleLinkedList.PushBack(50);
-	circleLinkedList.PushBack(60);
 
-	circleLinkedList.ShowNode();
 
 	
+
 	return 0;
 }
 
@@ -179,4 +24,9 @@ int main()
 
 
 	
+		
+		
+		
+		
+		
 
---
+++ class5th(LinearQueue)/Source5.cpp
@@ -0,0 +1,163 @@
+﻿#include<iostream>
+
+using namespace std;
+template <typename T>
+#define SIZE 5
+class Queue
+{
+private:
+
+	T front;
+	T rear;
+	int size;
+	T QueueArr[5] = { 0 };
+
+public:
+	Queue()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			QueueArr[i] = NULL;
+		}
+		front = 0;
+		rear = 0;
+		size = 0;
+
+	}
+	~Queue()
+	{
+
+		cout << endl << "Release Queue" << endl;
+	}
+	void Push(T data)
+	{
+
+		if (IsFull())
+		{
+			cout << "Queue is Full" << endl;
+
+
+		}
+		else
+		{
+			QueueArr[rear] = data;
+			rear++;
+			size++;
+		}
+
+
+	}
+
+
+
+
+
+
+
+	void Pop()
+	{
+		cout << "Remaining Arr : " << size << endl;
+		if (Empty())
+		{
+			cout << "Release Arr" << endl;
+
+		}
+		else
+		{
+			QueueArr[front] = NULL;
+			front++;
+			size--;
+		}
+	}
+	bool IsFull()
+	{
+		if (SIZE <= rear)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool Empty()
+	{
+		if (front == rear)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	T& Front()
+	{
+		if (Empty())
+		{
+			exit(1);
+		}
+		else
+		{
+
+			return QueueArr[front];
+		}
+	}
+	int& Size()				// '&' 사용해주면서 복사되는 비용을 안쓰게됌. 반환되는 값 size가 같이 들어가게됌 
+	{						// 기존의 int Size는 return 하면서 size 값을 복사하여 반환하기 때문에 복사비용이 생김.
+		return size;
+	}
+
+
+
+};
+
+
+int main()
+{
+#pragma region 선형구조 Queue
+	// 배열의 공간에 들어간 데이터가 고정되어 
+	// 데이터를 빼내더라도 초기화하지 않으며,
+	// 원래 데이터가 있던 배열의 자리에 더이상 다른 값이 
+	// 들어올 수 없는 Queue.
+
+	Queue <int>queue;
+	queue.Push(10);
+	queue.Push(20);
+	queue.Push(30);
+	queue.Push(40);
+	queue.Push(50);
+
+
+
+	cout << "Queue.Size : " << queue.Size() << endl;
+	while (queue.Empty() == false)
+	{
+		cout << queue.Front() << endl;
+		queue.Pop();
+	}
+
+
+
+#pragma endregion
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
