b7449cf5d713e76d910e7daa96cf500387799d07
hallow0312 <leejaehyun0312@naver.com>
2024-03-20 21:46:07 +0900

    2024.03.20연산자오버로딩

--- ProgramLEE/ProgramLEE.cpp
+++ ProgramLEE/ProgramLEE.cpp
@@ -2,17 +2,102 @@
 
 using namespace std;
 
+class Vector2
+{
+public:
+	int x;
+	int y;
 
+	Vector2(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+	Vector2 operator+(const Vector2& vector2)
+	{
+		Vector2 other
+		(
+			this->x + vector2.x,
+			this->y + vector2.y
+		);
+		return other;
+	}
+	Vector2 operator-(const Vector2& vector2)
+	{
+		Vector2 other
+		(
+			this->x - vector2.x,
+			this->y - vector2.y
+		);
+		return other;
+	}
 
+	Vector2 operator*(int value)
+	{
+		Vector2 other
+		(
+			this->x * value,
+			this->y * value
+		);
+		return other;
+	}
+	Vector2 operator/(const Vector2& vector2)
+	{
+		Vector2 other
+		(
+			this->x / vector2.x, 
+			this->y / vector2.y
+		); 
+		return other;
+	}
+};
+	
+Vector2 operator*(int value, Vector2 vector2)
+{
+	return vector2 * value;
+}
 		
 int main()
 {
+	Vector2 vector1(10, 20);
+	Vector2 vector2(5, 5);
+	
+	Vector2 vector3 = vector1 + vector2;
+	Vector2 vector4 = vector1 - vector2;
+	Vector2 vector5 = 10*vector1 ; 
+	Vector2 vector6 = vector1 / vector2;
+	Vector2 vector7 = vector1*12;
+
+
+	cout << "vector3의 값 : (" << vector3.x <<"," << vector3.y << ")"<<endl;
+	cout << "vector4의 값 : (" << vector4.x << "," << vector4.y << ")" << endl;
+	cout << "vector5의 값 : (" << vector5.x << "," << vector5.y << ")" << endl;
+	cout << "vector6의 값 : (" << vector6.x << "," << vector6.y << ")" << endl;
+	
+	cout << "vector7의 값 : (" << vector7.x << "," << vector7.y << ")" << endl;
 
 
 	
 
 	return 0;
 }
+	
+	
+	
+	
+
+	
+
+
+
+
+	
+
+
+
+
+	
+
 
 
 
@@ -29,4 +114,3 @@ int main()
 		
 		
 		
-
---
+++ class6th(CircleQueue)/Source6.cpp
@@ -0,0 +1,187 @@
+﻿#include<iostream>
+
+using namespace std;
+template <typename T>
+#define SIZE 4
+#pragma region 원형 큐
+//물리적으로는 선형 구조를 가지고 있으며,
+// 큐의 시작점과 끝점을 연결한 Queue
+class CircleQueue
+{
+private:
+	int front;
+	int rear;
+	T buffer[SIZE];
+
+
+public:
+	CircleQueue()
+	{
+		front = SIZE - 1;
+		rear = SIZE - 1;
+		for (int i = 0; i < SIZE; i++)
+		{
+			buffer[i] = NULL;
+		}
+
+
+	}
+	~CircleQueue()
+	{
+
+	}
+
+
+	void Push(T data)
+	{
+
+		if (IsFull())
+		{
+			cout << "Queue is full" << endl;
+		}
+		else
+		{
+
+			rear = (rear + 1) % SIZE;
+			buffer[rear] = data;
+
+		}
+
+
+	}
+	void Pop()
+	{
+
+		if (Empty())
+		{
+			cout << endl << "Release Arr" << endl;
+
+		}
+		else
+		{
+			front = (front + 1) % SIZE;
+			buffer[front] = NULL;
+
+
+		}
+	}
+
+
+
+	bool IsFull()
+	{
+		if (front == (rear + 1) % SIZE)
+		{
+			return true;
+
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool Empty()
+	{
+		if (front == rear)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	T& Front()
+	{
+
+		return buffer[(front + 1) % SIZE];
+
+
+
+	}
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#pragma endregion
+
+
+
+int main()
+{
+	CircleQueue <char> circlequeue;
+
+	circlequeue.Push('a');
+	circlequeue.Push('b');
+	circlequeue.Push('c');
+	circlequeue.Push('d');
+	while (circlequeue.Empty() == false)
+	{
+		cout << circlequeue.Front() << endl;
+		circlequeue.Pop();
+
+	}
+	circlequeue.Push('a');
+	circlequeue.Push('b');
+	circlequeue.Push('c');
+
+	while (circlequeue.Empty() == false)
+	{
+		cout << circlequeue.Front() << endl;
+		circlequeue.Pop();
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
