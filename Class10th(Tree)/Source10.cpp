cc80401b5fe69f22b473ea4ca7db8706a85de5f4
hallow0312 <leejaehyun0312@naver.com>
2024-03-26 21:47:57 +0900

    2024.03.26

---
+++ Class10th(Tree)/Source10.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};
+
+
+Node* CreateNode(int data, Node* left, Node* right)
+{	//1. 새로운 노드 작성
+	Node* newNode = new Node;
+
+	//2. 새로운 노드의 데이터 값 저장 
+	newNode->data = data;
+
+	//3. 새로운 노드의 left 값 저장 
+	newNode->left = left;
+
+	//4. 새로운 노드의 right 값 저장 
+	newNode->right = right;
+
+	//5. 새로운 노드의 주소값 반환
+	return newNode;
+}
+
+void Preivous(Node* node)
+{
+	if (node != nullptr)
+	{
+		cout << node->data << " ";
+		Preivous(node->left);
+		Preivous(node->right);
+	}
+	else
+	{
+		return;
+	}
+
+}
+void Inorder(Node* node)
+{
+	if (node != nullptr)
+	{
+		Inorder(node->left);
+		cout << node->data << " ";
+		Inorder(node->right);
+	}
+	else
+	{
+		return;
+	}
+}
+void Postorder(Node* node)
+{
+	if (node != nullptr)
+	{
+		Postorder(node->left);
+		Postorder(node->right);
+		cout << node->data << " ";
+	}
+	else
+	{
+		return;
+	}
+}
+
+int main()
+{
+
+#pragma region Tree 구조
+	//root : 최상위 노드															
+	//edge : 노드와 노드를 연결하는 간선											
+	//parent : 부모 노드 A B C (자신보다 하위 노드를 가지고 있는 노드 )			   
+	//leaf: 자식 노드를 가지고 있지 않는 노드 D E F G								
+	//level : root 로 부터 하위 단계를 측정하는 것.
+	//height: level 의 역순 
+	// path: 노드 와 노드 사이간에 나타나는 노드와 간선의 순서 
+	// degree: 해당노드가 포함하는 자식 노드의 수 
+	// 트리구조 사용 이유 : 반복문보다 시간복잡도에 의해 시간 감소 
+
+	Node* node7 = CreateNode(7, nullptr, nullptr);
+	Node* node6 = CreateNode(6, nullptr, nullptr);
+	Node* node5 = CreateNode(5, nullptr, nullptr);
+	Node* node4 = CreateNode(4, nullptr, nullptr);
+	Node* node3 = CreateNode(3, node6, node7);
+	Node* node2 = CreateNode(2, node4, node5);
+	Node* node1 = CreateNode(1, node2, node3);
+
+	Preivous(node1);
+	cout << endl;
+
+	Inorder(node1);
+	cout << endl;
+
+	Postorder(node1);
+	cout << endl;
+	//전위 순회
+	//  1.Root Node 방문 
+	//	2.왼쪽 서브 트리 전위 순회 
+	//	3. 오른쪽 서브 트리 전위 순회 
+	// 1->2->4->5->3->6->7
+
+	//중위 순회 
+	//1. 왼쪽 서브 트리 중위 순회
+	//2. Root Node 방문
+	//3. 오른쪽 서브 트리 중위 순회 
+
+	// 후위 순회 
+	// 1. 왼쪽 서브트리  후위 순회
+	//	2. 오른쪽 서브트리 후위 순회
+	//	3. Root Node 방문 
+#pragma endregion
+
+
+
+	return 0;
+}
--- ProgramLEE/ProgramLEE.cpp
+++ ProgramLEE/ProgramLEE.cpp
@@ -1,121 +1,150 @@
 ﻿#include <iostream>
-
 using namespace std;
 
-struct Node
+#define Max_Size 8
+template <typename T>
+class Heap
 {
-	int data;
-	Node* left;
-	Node* right;
-};
-
-
-Node* CreateNode(int data, Node*left, Node*right)
-{	//1. 새로운 노드 작성
-	Node* newNode = new Node;
-
-	//2. 새로운 노드의 데이터 값 저장 
-	newNode->data = data; 
-
-	//3. 새로운 노드의 left 값 저장 
-	newNode->left = left; 
-
-	//4. 새로운 노드의 right 값 저장 
-	newNode->right = right; 
-
-	//5. 새로운 노드의 주소값 반환
-	return newNode;
-}
-
-void Preivous(Node* node)
-{
-	if (node != nullptr)
-	{	
-		cout<<node->data<<" ";
-		Preivous(node->left);
-		Preivous(node->right);
-	}
-	else
-	{
-		return;
-	}
+private:
+	int index;
+	T buffer[Max_Size];  
 	
-}
-void Inorder(Node* node)
-{
-	if (node != nullptr)
+
+public: 
+	Heap()
 	{
-		Inorder(node->left);
-		cout << node->data<<" ";
-		Inorder(node->right);
+		index = 0; 
+		
+		for (int i = 0; i < Max_Size; i++)
+		{
+			buffer[i] = NULL; 
+		}
 	}
-	else
+	void Insert(int data)
 	{
-		return;
+		if (index >= Max_Size-1)
+		{	
+			cout << "Heap is Full" << endl;
+			return;
+		}
+		buffer[++index] = data;
+		
+
+		int child = index;
+		int parent = index / 2;
+		while (child> 1 && buffer[child] > buffer[parent])
+		{
+			/*
+			자식 노드와 부모 노드의 데이터를 비교 (&& 연산자를 안쓸거면)
+			if(buffer[parent]<buffer[child])
+			{
+				swap(buffer[parent,buffer[child])
+			}
+
+			*/
+			swap(buffer[child], buffer[parent]);
+
+			child = child / 2;
+
+			parent = child / 2;
+		}
+		
 	}
-}
-void Postorder(Node* node)
-{
-	if (node != nullptr)
+	T &Delete()
+	{	
+		//1. Heap 이 비어 있더만 프로그램 종료 
+		if (index<=0)
+		{	
+			cout << "Heap is empty" << endl;
+			exit(1);
+		}
+		
+		// 임시 변수에 buffer[1] 보관
+		T result = buffer[1];  
+		
+		buffer[1] = buffer[index];
+
+		buffer[index--] = NULL;
+		
+		int parent = 1;
+		int child = parent * 2;
+			
+		while (parent>=index)
+		{	
+			if (buffer[child + 1] > buffer[child])
+			{
+				++child;
+			}
+			
+			
+			if (buffer[parent] < buffer[child])
+			{
+				swap(buffer[parent], buffer[child]);
+			}
+			else
+			{
+				break;
+			}
+			parent = child;
+			child = parent * 2;
+		
+			
+		}
+		return result; 
+			
+
+		//2.index 로 가리키는 배열의 값을 첫번 째로 원소에 넣어줌
+		//index로 가리키는 배열의 값 초기화
+		// index  감소 
+	}
+		
+
+	void Show()
 	{
-		Postorder(node->left);
-		Postorder(node->right);
-		cout << node->data<<" ";
+		for (T element : buffer)
+		{
+			cout << element << " ";
+		}
+		
+
+
+
 	}
-	else
-	{
-		return;
-	}
-}
+
+		
+	
+		
+
+};
 
 int main()
 {
 	
-#pragma region Tree 구조
-	//root : 최상위 노드															
-	//edge : 노드와 노드를 연결하는 간선											
-	//parent : 부모 노드 A B C (자신보다 하위 노드를 가지고 있는 노드 )			   
-	//leaf: 자식 노드를 가지고 있지 않는 노드 D E F G								
-	//level : root 로 부터 하위 단계를 측정하는 것.
-	//height: level 의 역순 
-	// path: 노드 와 노드 사이간에 나타나는 노드와 간선의 순서 
-	// degree: 해당노드가 포함하는 자식 노드의 수 
-	// 트리구조 사용 이유 : 반복문보다 시간복잡도에 의해 시간 감소 
+#pragma region heap
+	//가장 큰값이 꼭대기에 
+	// 부모노드의 key 값이 자식노드의 key값보다 커야함
+	// 완전 이진 트리 
+	// 데이터 삽입이나 삭제가능 하나 완전이진트리가 유지가 되어야함 .
+	// 데이터를 뽑을 때 상수값이 들기에 시간 효율 좋음 
 
-	Node* node7 = CreateNode(7, nullptr, nullptr); 
-	Node* node6 = CreateNode(6, nullptr, nullptr); 
-	Node* node5 = CreateNode(5, nullptr, nullptr); 
-	Node* node4 = CreateNode(4, nullptr, nullptr); 
-	Node* node3 = CreateNode(3, node6, node7); 
-	Node* node2 = CreateNode(2, node4, node5); 
-	Node* node1 = CreateNode(1, node2, node3); 
-	
-	Preivous(node1);
+	//왼쪽 자식의 인덱스 = 부모노드의 인덱스 *2;
+	//오른쪽 자식의 인덱스 = 부모노드의 인덱스 *2+1;
+	//부모의 인덱스 = 자식의 인덱스 /2;
+	Heap <int> heap;
+	heap.Insert(10);
+	heap.Insert(20);
+	heap.Insert(30);
+	heap.Insert(40);
+	heap.Insert(50);
+	heap.Insert(60);
+	heap.Insert(70);
+	heap.Show();
+	heap.Delete();
 	cout << endl;
-	
-	Inorder(node1);
-	cout << endl;
-	
-	Postorder(node1);
-	cout << endl;
-	//전위 순회
-	//  1.Root Node 방문 
-	//	2.왼쪽 서브 트리 전위 순회 
-	//	3. 오른쪽 서브 트리 전위 순회 
-	// 1->2->4->5->3->6->7
+	heap.Show();
 
-	//중위 순회 
-	//1. 왼쪽 서브 트리 중위 순회
-	//2. Root Node 방문
-	//3. 오른쪽 서브 트리 중위 순회 
-	
-	// 후위 순회 
-	// 1. 왼쪽 서브트리  후위 순회
-	//	2. 오른쪽 서브트리 후위 순회
-	//	3. Root Node 방문 
 #pragma endregion
 
-	
 
 	return 0;
 }
